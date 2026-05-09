04fd148798ede2bc6c1b79c0f56f1f17af5448bc
tungbi811 <tungduynguyen811@gmail.com>
2026-05-09 22:34:57 +1000

    update ingestion

--- ingestion/03_product_crawler/extract_product_urls.js
+++ ingestion/03_product_crawler/extract_product_urls.js
@@ -1,48 +1,53 @@
-const host = process.env.VM_EXTERNAL_IP || "localhost";
-db = connect(`mongodb://${host}:27017/countly`);
-
 print("Starting aggregation...");
 
 db.summary.aggregate([
-  { $match: {
-    collection: { $in: [
-      "view_product_detail",
-      "select_product_option",
-      "select_product_option_quality",
-      "add_to_cart_action",
-      "product_detail_recommendation_visible",
-      "product_detail_recommendation_noticed",
-      "product_view_all_recommend_clicked"
-    ]}
-  }},
-  { $project: {
-    product_id: {
-      $cond: {
-        if: { $eq: ["$collection", "product_view_all_recommend_clicked"] },
-        then: "$viewing_product_id",
-        else: "$product_id"
-      }
-    },
-    url: {
-      $cond: {
-        if: { $eq: ["$collection", "product_view_all_recommend_clicked"] },
-        then: "$referrer_url",
-        else: "$current_url"
+  {
+    $match: {
+      collection: {
+        $in: [
+          "view_product_detail",
+          "select_product_option",
+          "select_product_option_quality",
+          "add_to_cart_action",
+          "product_detail_recommendation_visible",
+          "product_detail_recommendation_noticed",
+          "product_view_all_recommend_clicked"
+        ]
       }
     }
-  }},
-  { $group: {
-    _id: "$product_id",
-    url: { $first: "$url" }
-  }},
-  { $project: {
-    _id: 0,
-    product_id: "$_id",
-    url: 1
-  }},
+  },
+  {
+    $project: {
+      product_id: {
+        $cond: {
+          if: { $eq: ["$collection", "product_view_all_recommend_clicked"] },
+          then: "$viewing_product_id",
+          else: "$product_id"
+        }
+      },
+      url: {
+        $cond: {
+          if: { $eq: ["$collection", "product_view_all_recommend_clicked"] },
+          then: "$referrer_url",
+          else: "$current_url"
+        }
+      }
+    }
+  },
+  {
+    $group: {
+      _id: "$product_id",
+      url: { $first: "$url" }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      product_id: "$_id",
+      url: 1
+    }
+  },
   { $out: "product_urls" }
 ]);
 
-print("Done! Count: " + db.product_urls.countDocuments());
-print("Sample:");
-printjson(db.product_urls.findOne());
+print("Done! Number of URL: " + db.product_urls.countDocuments());
